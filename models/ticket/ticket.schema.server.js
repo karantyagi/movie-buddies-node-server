b1fed8d871db5e4f17b8164e32b5267b5d1517d3
Karan Tyagi <contactkarantyagi@gmail.com>
2018-12-11 15:17:29 -0500

    fixed ticket

--- models/ticket/ticket.schema.server.js
+++ models/ticket/ticket.schema.server.js
@@ -3,5 +3,5 @@ const ticketSchema = mongoose.Schema({
     booking: {type: mongoose.Schema.Types.ObjectId, ref: 'BookingModel'},
     event: {type: mongoose.Schema.Types.ObjectId, ref: 'EventModel'},
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'}
-}
+});
 module.exports = ticketSchema;
